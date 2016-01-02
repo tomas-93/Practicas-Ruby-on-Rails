de1b91d97e88e2b13f0b9ae440e5b8233ba8bba3
Tomas Yussef Galicia Guzman <tomasyussef@gmail.com>
2016-01-01 22:44:06 -0600

    Revert "Practica 4"
    This reverts commit fe02edacb01ac361e0d37c6284d3069af18ad8b0.

--- Practica 4 Cookie y Sesion/test/controllers/test_cookies_controller_test.rb
+++
@@ -1,9 +0,0 @@
-require 'test_helper'
-
-class TestCookiesControllerTest < ActionController::TestCase
-  test "should get identify" do
-    get :identify
-    assert_response :success
-  end
-
-end
--- Practica 4 Cookie y Sesion/test/helpers/test_cookies_helper_test.rb
+++
@@ -1,4 +0,0 @@
-require 'test_helper'
-
-class TestCookiesHelperTest < ActionView::TestCase
-end
